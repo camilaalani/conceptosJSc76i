bbe3b4454b8b0810988636530a2758de7d017906
Camila Alani <alanicamila124@gmail.com>
2023-10-19 18:15:53 -0300

    agregamos un ejemplo de prompt con parseInt

--- js/1-variables.js
+++ js/1-variables.js
@@ -28,8 +28,15 @@ document.write("Año: "+anioActual+ "<br>");
 document.write("Año: "+anioActual);
 
 //ventanas emergentes
-alert("Esto es una prueba de alerta");
+//alert("Esto es una prueba de alerta");
 
 let color = prompt("Ingrese un color");
 console.log(color);
 
+// solicitar al usuario un numero, y al numero indicado por el usuario multiplicaarle 15
+let numeroSeleccionado = parseInt(prompt("Ingrese un numero de 1 a 100"));
+console.log(numeroSeleccionado);
+console.log(numeroSeleccionado + 15);
+
+// parseInt("10") devuelve 10
+// parseFloat("10.5") devuelve 10.5
