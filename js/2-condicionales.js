311bb23bc64b055a8c6b3e1df78e25c59c65c32b
Camila Alani <alanicamila124@gmail.com>
2023-10-19 18:36:42 -0300

    agregamos un archivo de condicionales e hicimos un ejercicio con condicionales lógicas if

---
+++ js/2-condicionales.js
@@ -0,0 +1,13 @@
+/* if(condición logica){
+    codigo a ejecutar si se cumple la condición lógica
+}
+*/
+
+// 7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+
+let numero1 = parseInt(prompt("Ingrese un primer numero"));
+let numero2 = parseInt(prompt("Ingrese un segundo numero"));
+
+if( numero1 > numero2){
+    console.log("La variable numero 1 es mayor a la variable 2")
+}
